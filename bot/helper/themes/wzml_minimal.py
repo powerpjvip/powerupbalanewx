15ec6c11eb85c0f7423cb833b6836c28418151ac
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-04-21 08:43:42 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -178,7 +178,9 @@ class WZMLStyle:
 
     #####---------PROGRESSIVE STATUS-------
     BAR =               '\n┝[{Bar}]'
+    PREC =              '\n┝<b>Total: </b> {Pre}
     PROCESSED =         '\n┝<b>Processed:</b> {Processed}'
+    TOTAL_SIZE =        '\n┝<b>Total :</b> {Total_s}
     STATUS =            '\n┝<a href="{msg_link}">{Status}</a>'
     ETA =               '\n┝<b>ETA:</b> {Eta}'
     SPEED =             '\n┝<b>Speed:</b> {Speed}'
